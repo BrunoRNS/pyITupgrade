8e910b7a011a7ac6b5dee75661aebb5febce7344
BrunoRNS <brunorns05@outlook.com>
2026-07-13 12:46:08 -0300

    Fix pattern truncate on create_sample_it

--- test/create_sample_it/create_sample_it.py
+++ test/create_sample_it/create_sample_it.py
@@ -11,13 +11,19 @@ if str(SRC_ROOT) not in sys.path:
 from pyIT import PatternBuilder, ITfile, WavInstrumentBuilder, getAsset
 
 sonic_like_pattern: List[Any] = [
-    "C-5", None, "A-4", None, None,
-    "C-5", None, "B-4", None, "C-5",
-    "B-4", "G-4", None, None, None, "A-4", None,
-    "E-5", None, "D-5", None, None, "C-5", None, "B-4", None, "C-5",
-    "B-4", "G-4", None, None, None, "A-4", None,
-    "F-4", None, "A-4", None, "G-4", None, "A-4", None,
-    "G-4", "E-4", None, None, None
+    "C-5", "A-4", None,
+    "C-5", "B-4", None, 
+    "C-5", "B-4", None, "G-4", None, None,
+    "A-4", None, "E-5", "D-5", None, 
+    "C-5", "B-4", None, 
+    "C-5", "B-4", None, "G-4", None, None,
+    "A-4", "F-4", None,
+    "A-4", "G-4", None, 
+]
+
+sonic_like_pattern_2: List[Any] = [
+    "A-4", "G-4", None, "E-4", None, None,
+    None,
 ]
 
 def create_sample_it(it_path: Path) -> None:
@@ -25,8 +31,10 @@ def create_sample_it(it_path: Path) -> None:
     music = ITfile()
     music.SongName = "SONIC_LIKE"
     
-    pbuilder = PatternBuilder()
-    pattern = pbuilder.build_pattern(sonic_like_pattern, instrument_id=1)
+    pbuilder = PatternBuilder(bpm=180, lines_per_note=2)
+    
+    pattern1 = pbuilder.build_pattern(sonic_like_pattern, instrument_id=1)
+    pattern2 = pbuilder.build_pattern(sonic_like_pattern_2, instrument_id=1)
     
     instrument, sample = WavInstrumentBuilder.create_from_wav(
         str(getAsset(id=2).resolve()), instrument_name="Square"
@@ -37,7 +45,9 @@ def create_sample_it(it_path: Path) -> None:
     
     music.Samples.append(sample)
     music.Instruments.append(instrument)
-    music.Patterns.append(pattern)
+    
+    music.Patterns.append(pattern1)
+    music.Patterns.append(pattern2)
     
     music.Orders.extend([0, 255])
     
